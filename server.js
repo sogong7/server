568b13d46b3817f888af3d85e6e9fa388373142f
luceinaltis <luceinaltis2020@naver.com>
2021-12-19 11:43:09 +0900

    kihwan

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5000;
-var axios = require("axios").default;
+const port = 5000;
+const axios = require("axios").default;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,67 +19,94 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-var options = {
-  method: "GET",
-  url: "https://ott-details.p.rapidapi.com/advancedsearch",
-  params: {
-    start_year: "1970",
-    end_year: "2020",
-    min_imdb: "6",
-    max_imdb: "7.8",
-    genre: "action",
-    language: "english",
-    type: "movie",
-    sort: "latest",
-    page: "1",
-  },
-  headers: {
-    "x-rapidapi-host": "ott-details.p.rapidapi.com",
-    "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
-  },
-};
+app.get("/", (req, res) => {
+  console.log("helath!");
+  res.send("health!");
+});
 
-var popular_api_options = {
-  method: "GET",
-  url: "https://movies-tvshows-data-imdb.p.rapidapi.com/",
-  params: { type: "get-popular-movies", page: "1", year: "2020" },
-  headers: {
-    "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
-    "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
-  },
-};
-
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-app.get("/api/recommend", (req, res) => {
+app.get("/api/recommend", async (req, res) => {
   /* TODO: 추천 결과 정보 */
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fileds) => {
-    res.send(rows);
-  });
-});
 
-app.get("/api/top10", (req, res) => {
-  /* https://rapidapi.com/amrelrafie/api/movies-tvshows-data-imdb/ */
-  /* TODO: 인기있는 영화 TOP10 정보 */
+  var options = {
+    method: "GET",
+    url: "https://movies-tvshows-data-imdb.p.rapidapi.com/",
+    params: { type: "get-random-movies", page: "1" },
+    headers: {
+      "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+      "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
+    },
+  };
   try {
+    let response = await axios.request(options);
+    res.send(response.data);
   } catch (error) {
-    console.error("🤬 삐빕!");
+    console.error("🤬 에러!");
+    res.send("🤬 에러!").end();
   }
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fileds) => {
-    res.send(rows);
-  });
 });
 
-app.get("/api/detail/{movieId}", (req, res) => {
-  /* TODO: 영상 상세 정보 */
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fileds) => {
-    res.send(rows);
-  });
+app.get("/api/top10", async (req, res) => {
+  /* 인기있는 영상 TOP10 정보 */
+  const options = {
+    method: "GET",
+    url: "https://movies-tvshows-data-imdb.p.rapidapi.com/",
+    params: { type: "get-popular-movies", page: "1", year: "2021" },
+    headers: {
+      "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+      "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
+    },
+  };
+
+  try {
+    let response = await axios.request(options);
+    res.send(response.data);
+  } catch (error) {
+    console.error("🤬 에러!");
+    res.send("🤬 에러!").end();
+  }
+});
+
+app.get("/api/poster/:movieId", async (req, res) => {
+  /* 영상 포스터 정보 */
+  const movieId = req.params.movieId;
+  const options = {
+    method: "GET",
+    url: "https://movies-tvshows-data-imdb.p.rapidapi.com/",
+    params: { type: "get-movies-images-by-imdb", imdb: `${movieId}` },
+    headers: {
+      "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+      "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
+    },
+  };
+  try {
+    let response = await axios.request(options);
+    res.send(response.data);
+  } catch (error) {
+    console.error("🤬 에러!");
+    res.send("🤬 에러!").end();
+  }
+});
+
+app.get("/api/detail/:movieId", async (req, res) => {
+  /* 영상 상세 정보 */
+  var options = {
+    method: "GET",
+    url: "https://movies-tvshows-data-imdb.p.rapidapi.com/",
+    params: { type: "get-movie-details", imdb: `${movieId}` },
+    headers: {
+      "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+      "x-rapidapi-key": "7c1c27a5b4msh9043c7fd8d0acc0p18fa7ejsn189151ac97e1",
+    },
+  };
+  try {
+    let response = await axios.request(options);
+    res.send(response.data);
+  } catch (error) {
+    console.error("🤬 에러!");
+    res.send("🤬 에러!").end();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server gets started at ${port}`);
 });
